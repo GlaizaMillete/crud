a03a82681bb94420cca256c7c624f9c5c336c625
Jaded <jaduya678@gmail.com>
2023-09-18 12:13:07 +0800

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { AuthComponent } from './auth/auth.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    PostListComponent,
+    AuthComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  n=0;
+  listofPosts: Post[]=[
+  new Post("The Verge", "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/12/the-verge-website-homepage.webp", "The Verge is a blog focused on examining how technology will change the future. This blog provides news and opinion pieces on the latest technological developments in art, culture, and science for the mainstream audience.", "Jade Raposa", 
+    new Date()),
+  new Post("Tech Crunch", "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/12/techcrunch-website-homepage.webp", "TechCrunch is a blog that provides technology and startup news, from the latest developments in Silicon Valley to venture capital funding.", "Christian Montesor", 
+    new Date())
+  ]
+  constructor() {}
+  
+
+  ngOnInit(): void{
+
+  }
+}
